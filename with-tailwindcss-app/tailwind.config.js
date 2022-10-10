d9ad2988bef6e8eda85a30e33b93d60b13dad794
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-09 23:26:30 -0400

    feat(style): add google font fam

--- with-tailwindcss-app/tailwind.config.js
+++ with-tailwindcss-app/tailwind.config.js
@@ -1,32 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  // theme: {
-  //   extend: {
-  //     animation: {
-  //       blob: "blob 7s infinite",
-  //     },
-  //     keyframes: {
-  //       blob: {
-  //         "0%": {
-  //           transform: "translate(0px, 0px) scale(1)",
-  //         },
-  //         "33%": {
-  //           transform: "translate(30px, -50px) scale(1.1)",
-  //         },
-  //         "66%": {
-  //           transform: "translate(-20px, 20px) scale(0.9)",
-  //         },
-  //         "100%": {
-  //           transform: "tranlate(0px, 0px) scale(1)",
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans]
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
   variants: {
     extend: {},
   },
