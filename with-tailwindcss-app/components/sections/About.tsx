2335300e9bc0ad502d2f59970824dfcfb60376bd
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-09 23:27:45 -0400

    fix(about): update about paragraphs

--- with-tailwindcss-app/components/sections/About.tsx
+++ with-tailwindcss-app/components/sections/About.tsx
@@ -17,21 +17,32 @@ function About() {
       </div>
 
       <div className="w-1/2 flex flex-col justify-center items-start">
-        <h1 className="text-4xl mb-3">Hi there!</h1>
+        <h1 className="text-4xl mb-3 text-sky-500">Hi there!</h1>
         <p className="text-sm w-3/4 mb-2 leading-6">
-          My name is Charlie Carr and I am a NYC-based software developer who is
-          actively pursuing full time employment and open to both remote and
+          My name is Charlie Carr and I am a NYC-based software developer. I am
+          currently pursuing my next opportunity and open to both remote and
           in-office work.
         </p>
+        <p className="text-sm w-3/4 mb-2 leading-6">
+          I have spent my career in tech working in enterprise software for both
+          small startups as well as large tech corporations. After starting in
+          software sales, my passion and curiosity for these products fueled my
+          desire to become more technical and attend a full stack web development
+          coding bootcamp.
+        </p>
         <p className="text-sm w-3/4 leading-6">
-            I have spent my career in tech working in enterprise software for
-            both small startups as well as large tech corporations. After
-            starting on the sales side of the industry, my passion and curiosity
-            for these products fueled my desire to become more technical and
-            attend an in person full stack coding bootcamp. Currently, I am
-            focused on front-end web development and expanding my knowledge in
-            Next.js and TypeScript.
-          </p>
+          I recently finished my first engineering role as a front end development
+          intern for{" "}
+          <a
+            href="https://www.reveliolabs.com/"
+            target="_blank"
+            className="underline text-sky-300"
+          >
+            Revelio Labs
+          </a>
+          , a labor market data platform, where I actively contributed to the
+          React/TypeScript code base.
+        </p>
       </div>
     </section>
   );
