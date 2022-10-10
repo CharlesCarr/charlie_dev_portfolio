ca12eee9521faba314bb16db981a3d1c9e8df5a3
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-10 19:37:12 -0400

    fix(footer): update footer responsiveness

--- with-tailwindcss-app/components/Footer.tsx
+++ with-tailwindcss-app/components/Footer.tsx
@@ -2,9 +2,9 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-black flex px-60 py-24 text-sm">
+    <div className="w-full bg-black flex px-2 sm:px-10 lg:px-60 py-24 text-xs sm:text-sm">
       <div className="flex flex-col items-start w-1/2">
-        <p className="text-xl mb-2">This Website</p>
+        <p className="text-base sm:text-xl mb-2">This Website</p>
         <p className="mb-2">Designed & Developed by Charlie Carr</p>
         <p className="mb-2">React, Next.js, TypeScript, Tailwind CSS</p>
         <div className="flex items-center hover:underline cursor-pointer">
@@ -14,7 +14,7 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col items-end w-1/2">
-        <p className="text-xl mb-2">Contact</p>
+        <p className="text-base sm:text-xl mb-2">Contact</p>
         <p className="mb-2">charliecarr4@gmail.com</p>
         <p className="mb-2">LinkedIn Profile</p>
         <p>GitHub Profile</p>
